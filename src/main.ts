566544eccad9eb8e2fa7aa32df47d4c5644ac9a7
ssxdtxr <tereskinegor68@gmail.com>
2025-11-26 11:39:01 +0300

    feat: add telegraf dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { initBot } from './bot';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-
-  console.log("Запустился на 3000 порту")
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log('Запустился на 3000 порту');
+  initBot();
 }
 bootstrap();
